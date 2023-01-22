bfcbebdeee86f845e744c9d29b2e9e6bb395b8d1
dave <dawid.strojek@gmail.com>
2023-01-22 19:25:21 +0100

    basic setting of username

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 // app.js
 var http = require('http');
-var fs = require('fs');
 var express = require('express');
-var homePage = fs.readFileSync('./views/app.ejs', 'utf-8');
 
 var app = express();
 var server = http.createServer(app);
 
 app.use(express.static('./static'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.urlencoded({extended:true}));
 
 app.get('/', function (req, res) {
-    res.setHeader('Content-type', 'text/html');
-    res.write(homePage);
+    res.render('app', { username: ''});
+    res.end();
+});
+
+app.post('/', (req, res) => {
+    var username = req.body.username;
+
+    if ( username && username.length >= 3 ) { 
+        res.redirect('/rooms');
+    } else {
+        res.render('app', { 
+            username: username, 
+            message: 'Username must contain at least 3 characters'
+        });
+    }
+});
+
+app.get('/rooms', function (req, res) {
+    res.render('rooms');
     res.end();
 });
 
